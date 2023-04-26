f214dd79f5280f2e89ac885228b3573dd464f981
khomotsos <ariellesethoga@gmail.com>
2023-04-26 15:40:41 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print-alphabet  -print in lower case
+ *
+ * return: 0 
+ *
+ */
+
+
+void print_alphabet(void)
+{	char la = 'a';
+
+
+	for (la = 'a'; la <= 'z'; la++)
+
+		_putchar(la);
+	
+
+	_putchar('\n');
