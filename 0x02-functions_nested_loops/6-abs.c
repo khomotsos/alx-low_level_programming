27fab29b9823d0d7062c0ec28e43f6aa100f5069
khomotsos <ariellesethoga@gmail.com>
2023-04-26 17:20:44 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - compute the absolute value of an interge
+ * @t: The interge to be computed
+ * Return: Absolute value
+ */
+int _abs(int t)
+{
+    if (t >= 0)
+	  return (t);
+    else
+	    return (-t);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,4 @@ int _isalpha(int c);
 int _islower(int c);
 void print_alphabet_x10(void);
 int print_sign(int n);
+int _abs(int)
